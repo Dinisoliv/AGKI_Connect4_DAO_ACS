b7bdd9ed5c4bb062c1cf96df986fb8a0b379a51b
AndreSilva-et <andre.castelhanos@ua.pt>
2026-01-30 19:42:05 +0100

    changed main

--- config.h
+++ config.h
@@ -2,15 +2,15 @@
 #define CONFIG_H
 
 
-#define DEPTH 3
+#define DEPTH 10
 
 #define RANDOM 1
 #define OPP RANDOM 
 
 #define NEGAMAX 1
 #define ALPHABETA 2
-#define ALPHABETATT 3
-#define ALG NEGAMAX 
+#define ALPHABETA_TT 3
+#define ALG ALPHABETA_TT 
 
 
 #endif
